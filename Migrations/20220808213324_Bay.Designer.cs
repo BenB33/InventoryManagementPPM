6640a0d0524131640a993ca8dfc1f10c328c2257
Jack <jabmilgate@live.com>
2022-08-08 22:34:17 +0100

    BAYS

--- Migrations/20220808213324_Bay.Designer.cs
+++ Migrations/20220808213324_Bay.Designer.cs
@@ -3,6 +3,7 @@ using InventoryManagmentPPM.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace InventoryManagmentPPM.Migrations
 {
     [DbContext(typeof(InventoryManagmentPPMContext))]
-    partial class InventoryManagmentPPMContextModelSnapshot : ModelSnapshot
+    [Migration("20220808213324_Bay")]
+    partial class Bay
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
