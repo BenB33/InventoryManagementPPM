fda7ad35b2b7be6d1cb1564940c0c8d0e3119e9f
Ben Harrison <benharrison9898@gmail.com>
2022-08-26 01:38:43 +0100

    Approve part 1

---
+++ Pages/Inventory/Approve.cshtml.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using InventoryManagmentPPM.Data;
+using InventoryManagmentPPM.Models;
+
+namespace InventoryManagmentPPM.Pages.Inventory
+{
+    public class ApproveModel : PageModel
+    {
+        private readonly InventoryManagmentPPM.Data.InventoryManagmentPPMContext _context;
+
+        public ApproveModel(InventoryManagmentPPM.Data.InventoryManagmentPPMContext context)
+        {
+            _context = context;
+        }
+
+        [BindProperty]
+        public InventoryItem InventoryItem { get; set; } = default!;
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null || _context.InventoryItem == null)
+            {
+                return NotFound();
+            }
+
+            var inventoryitem =  await _context.InventoryItem.FirstOrDefaultAsync(m => m.ID == id);
+            if (inventoryitem == null)
+            {
+                return NotFound();
+            }
+            InventoryItem = inventoryitem;
+            return Page();
+        }
+
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see https://aka.ms/RazorPagesCRUD.
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _context.Attach(InventoryItem).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!InventoryItemExists(InventoryItem.ID))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return RedirectToPage("./Index");
+        }
+
+        private bool InventoryItemExists(int id)
+        {
+          return (_context.InventoryItem?.Any(e => e.ID == id)).GetValueOrDefault();
+        }
+    }
+}
